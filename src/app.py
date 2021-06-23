1c99a9c6e9470ee21fd9910304ff6c463697b626
Ryan Gorden <ryangroden@gmail.com>
2021-06-22 19:09:19 -0500

    add database

--- src/app.py
+++ src/app.py
@@ -1,9 +1,17 @@
+import os
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from netmiko import ConnectHandler
 from napalm import get_network_driver
 
-app = Flask(__name__)
 
+# Create Flask application, load configuration, and
+# create the SQLAlchemy object
+basedir= os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(basedir, 'network.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db= SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET'])
