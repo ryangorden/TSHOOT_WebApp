1f1666e0094338c6bc4b915753e57a60f3f22489
Ryan Gorden <ryangroden@gmail.com>
2021-06-26 00:55:14 -0500

    cp test_home to src folder

---
+++ src/test_home.py
@@ -0,0 +1,15 @@
+import sys
+from unittest import TestCase
+
+
+#sys.path.append("/src")
+from app import app
+
+
+class TestHome(TestCase):
+    def test_home(self):
+        with app.test_client() as c:
+            resp= c.get('/')
+
+#            self.assertEqual(resp.text, {"home": "Hello Networkers"})
+            self.assertEqual(resp.status_code, 200)
